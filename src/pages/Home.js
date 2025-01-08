dd951eced5e2b9b8ad1ebda5c69a39677cf78dc5
Athul Babu <athul.6560@gmail.com>
2025-01-08 17:30:53 -0600

    texts done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,30 @@
-import React from 'react';
-
+import React from "react";
+import "./Home.css"; // Make sure to create a CSS file for styling
+import logo from "../img/zeeza-icon.png";
 const Home = () => {
   return (
-    <div>
-      <h2>Sorry, Maintanance Going on!!</h2>
-      <h3>Contact at info@zeezaglobal.com</h3>
+    <div className="home-container">
+      {/* Left Section */}
+      <div className="left-section">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1 className="heading">
+          Custom Software <br /> Solution
+        </h1>
+        <p className="subtext">
+          Ready to transform your vision into reality? Let’s create something
+          extraordinary together!
+        </p>
+      <h2 className="textButton">Contact Us<i className="fas fa-arrow-right"></i></h2>
+      </div>
+
+      {/* Right Section */}
+      <div className="right-section">
+        <div className="card">Card 1 Content</div>
+        <div className="card">Card 2 Content</div>
+        <div className="card">Card 3 Content</div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
