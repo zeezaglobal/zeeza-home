4f531f1e939be953f9a1bfa0f887fc2089bc552a
Athul Babu <43273242+athul6560@users.noreply.github.com>
2025-01-20 03:34:47 -0600

    Images are added (#4)
    * test commit
    
    * images all are changed
    
    ---------
    
    Co-authored-by: athul <zeeza.global@gmail.com>

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,7 +2,7 @@ import image1 from "../assets/images/demo-brutalist/portfolio/1.jpg";
 import image2 from "../assets/images/demo-brutalist/portfolio/2.jpg";
 import image3 from "../assets/images/demo-brutalist/portfolio/3.jpg";
 import image4 from "../assets/images/demo-brutalist/portfolio/4.jpg";
-import image5 from "../assets/images/demo-brutalist/portfolio/5.jpg";
+
 
 export const portfolios3 = [
   {
@@ -10,7 +10,7 @@ export const portfolios3 = [
     imgSrc: image1,
     imgWidth: 700,
     imgHeight: 848,
-    title: "Android Tablet POS System",
+    title: "POS System",
     description:
       "A robust point-of-sale system designed for Android tablets, streamlining restaurant operations and integrating seamlessly with thermal printers.",
   },
@@ -28,7 +28,7 @@ export const portfolios3 = [
     imgSrc: image3,
     imgWidth: 700,
     imgHeight: 848,
-    title: "Production Tracking System",
+    title: "Inventory Management",
     description:
       "An efficient system to monitor and manage production workflows, ensuring optimal performance and productivity across manufacturing units.",
   },
